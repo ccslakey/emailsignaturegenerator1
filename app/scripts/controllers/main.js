795607735ad53e1f02a670089bdb9a794e4670b9
ccslakey <cromwellslakey@gmail.com>
2015-07-27 11:43:31 -0700

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name signatureprojectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the signatureprojectApp
+ */
+angular.module('signatureprojectApp')
+  .controller('MainCtrl', function () {
+    // signature defaults
+    this.sigName = "Bob Bobertson"
+    this.sigNameLink = "google.com"
+
+
+
+  });
